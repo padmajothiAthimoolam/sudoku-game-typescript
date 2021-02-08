94aa3daff8ffd8d7091a8ffda74bd609468816c5
Padma jothi <padmajothi1603@gmail.com>
2021-02-07 23:05:16 -0500

    complete project commit

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+
+import { Card, Content, Grid, NewButton, Numbers, Title } from 'components'
+import { configureStore, unregister } from 'core'
+import { GlobalStyles, theme } from 'styles'
+
+const store = configureStore()
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Provider store={store}>
+      <Content data-cy="content">
+        <Title data-cy="title">Sudoku</Title>
+        <Card data-cy="card">
+          <NewButton />
+          <Grid />
+          <Numbers />
+        </Card>
+      </Content>
+    </Provider>
+  </ThemeProvider>,
+  document.getElementById('root')
+)
+
+unregister()
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    background: 'radial-gradient(#2D608Ccc, #4C052E)',
+    black: '#282c34',
+    blue: '#a0e9fd',
+    lightBlue: '#caf3fe',
+    white: 'white',
+  },
+  transition: '0.3s',
+}
